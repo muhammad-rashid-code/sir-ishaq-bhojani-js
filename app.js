12c45d827d05e5c6726b064c5eebd0c916e26606
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-04-18 07:14:42 +0500

    intialize project

---
+++ app.js
@@ -0,0 +1,114 @@
+// let                                      //variabl // var //let
+// alert("hellow world");
+
+// let firstname = "ishaq";               //variabl // var //let
+// alert(firstname)
+
+// variable rules
+// 1. legal or illegal
+// use camel case for long variables
+// i.e
+//legal
+// allowed special characters are - $
+//let lastName // camel case
+//let last_name // snake case
+//let $namewith$dollar="";
+//let _namewith_dollar="";
+//varibale cannot start with numbe
+//let firs8tname is ok
+// let 8firstname is wrong
+// alert (lastName )
+
+//illegal
+//spaces
+
+// alert()
+
+
+// smit js02-------------------------------------------------------------------------------
+// let data="hello ";
+// let data2="students";
+// let data3=data+data2;
+// console.log(data3);
+// strings concatnation
+// let data="hello ";
+// let data2="students";
+// let data3=data+data2;
+// console.log(data3);           // strings concatnation
+
+// let data=5;
+// let data1=1;
+// let data2=data+data1
+// let data2=data+data1;
+// console.log(data2);           no ""  no strings concatnation // additon will take place
+
+// let data= "hello students";
+// let data2=data.includes ("students");
+// console.log(data2);
+
+// let data="hello students";
+// let data1=data.includes("student");
+// console.log(data1);
+
+
+
+// diffrent addition method
+// let data="5";
+// let data1=1;
+// let data2=
+// console.log(data+data1);  
+
+
+// look down
+
+// let data="5";
+// let data1=1;
+// console.log(+data+data1);
+
+// 2 2
+
+// look down
+
+// let data="5";
+// let data1=1;
+// console.log(parseInt(data)+data1);
+
+//   3
+
+// look down
+
+
+// let data = "5";
+// let data1 = 2;
+// console.log(+(data)%data1);
+
+// well we are using math oprator where + is math oprator we canperform +-*/%
+
+
+// let data = "5";
+// let data1 = 2;
+// console.log(+(data)+data1);
+
+// let data = "5";
+// let data1 = 2;
+// console.log(+(data)/data1);
+
+
+
+// let data="5";
+// let data1=1;
+// console.log(parseInt(data)-data1);
+// well we are using math oprator where "parseInt" is math oprator we canperform +-*/%
+
+
+// here we have % is known as modulo or remaider
+
+// let data="12";
+// let data1=4;
+// console.log(parseInt(data)%data1);  in this we have remainder 0
+
+
+// let data="11";
+// let data1=4;
+// console.log(parseInt(data)%data1);   in this we have remainder 3
+
