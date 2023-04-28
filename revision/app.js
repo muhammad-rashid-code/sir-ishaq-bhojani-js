db16c2bad792aa0ed18390e87bef547661d98247
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-04-29 03:36:15 +0500

    commit tree

---
+++ revision/app.js
@@ -0,0 +1,66 @@
+// alert("hello rashid")
+
+
+//chapte#1___________ALERT
+
+//alert("Hello Rahid")
+//alert('Hello Rahid')
+
+// ______________types of data in java script 
+//1. //string example ("hello rashid") or "abcd@"
+
+//2. numbers 5 8 12 9 -5 .008
+
+// bolean  True & False
+
+//reserved words java script___________alert|console|var|let
+//chapte#2________________________________________ variable
+
+// var greeting=("Hello Rashid");
+// var num=29;
+// var bolean=false;
+// alert(bolean);
+
+// var greeting=("Hello Rashid");
+// var num=29;
+
+// alert(num);
+
+// var greeting=("Hello Rashid");
+
+
+// alert(greeting);
+
+//ORRRRRRRR______________ partiton concept
+// var greeting;
+// greeting=("Hello Rashid")
+
+// alert(greeting)
+
+//chapter #3 variable name LEGAL and ILLEGAL
+//1. varibale  space not allowed
+// var hello world=("Hello World")  ILLEgal case
+
+// var hello_world=("Hello World")  LEgal case |underscore_case 
+// var helloWorld=("Hello World")  LEgal case |camelCase
+
+// where is my bag (convert into camel case)
+// whereIsMyBag"camelCase"
+
+
+// Make your variable names descriptive, so it's easier to figure out what your code means
+// when you or someone else comes back to it three weeks or a year from now. Generally,
+// userName is better than x, and faveBreed is better than favBrd, though the shorter names
+// are perfectly legal. You do have to balance readability with conciseness, though.
+// bestSupportingActressInADramaOrComedy is a model of clarity, but may be too much
+// for most of us to type or read. I'd shorten it.
+
+//Example
+
+// var =may
+// var month=("may")
+
+// var =2023
+// var year=("2023")
+
+// chapter#5 math expressions familar oprations
