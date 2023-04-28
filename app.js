d6637b2e15d8e10d3917eff08ffcee5d9ad31f75
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-04-29 02:23:05 +0500

    commit tree

--- app.js
+++ app.js
@@ -361,17 +361,295 @@
 
 
 
-var contactNumbers = [];
-var contactNames = [];
+// var contactNumbers = [];
+// var contactNames = [];
 
-for (var i = 0; i < 3; i++) {
-  var number = prompt("Enter your contact number:");
-  var name = prompt("Enter your contact name:");
-  contactNumbers.push(number);
-  contactNames.push(name);
-}
+// for (var i = 0; i < 3; i++) {
+//   var number = prompt("Enter your contact number:");
+//   var name = prompt("Enter your contact name:");
+//   contactNumbers.push(number);
+//   contactNames.push(name);
+// }
 
-for (var i = 0; i < contactNumbers.length; i++) {
-  console.log(`Contact ${i+1}: ${contactNumbers[i]} (${contactNames[i]})`);
-  alert(`Contact ${i+1}: ${contactNumbers[i]} (${contactNames[i]})`);
-}
+// for (var i = 0; i < contactNumbers.length; i++) {
+//   console.log(`Contact ${i+1}: ${contactNumbers[i]} (${contactNames[i]})`);
+//   alert(`Contact ${i+1}: ${contactNumbers[i]} (${contactNames[i]})`);
+// }
+
+// var products = ['Apple', 'Hockey', 'Orange', 'Cat', 'watermelon'];
+// var position = Number(prompt("Enter the position of the item you want to remove:\n0. Apple  \n1.Hockey  \n2.Orange \n3.Cat \n4.watermelon"));
+
+// var removedItem = products.splice(position, 1)[0];
+// console.log(`The removed item is ${removedItem}`);
+// alert(`The removed item is ${removedItem}`);
+
+// console.log(`The remaining items are ${products}`);
+// alert(`The remaining items are ${products}`);
+
+// console.log(`The total number of remaining items is ${products.length}`);
+// alert(`The total number of remaining items is ${products.length}`);
+
+// var nationality = prompt("What is your nationality?");
+// var gender = prompt("What is your gender?");
+// var age = parseInt(prompt("What is your age?"));
+
+// if (nationality === "Pakistani" || nationality === "pakistani" || nationality === "PAKISTANI" || nationality === "Indian" || nationality === "indian" || nationality === "INDIAN") {
+//     if (gender === "male" || gender === "Male" && age >= 18) {
+//         console.log("You are eligible to vote.");
+//         alert("You are eligible to vote.");
+//     } else if (gender === "female" || gender === "Female" && age >= 18) {
+//         var married = prompt("Are you married? (yes or no)");
+//         if (married === "yes" || married === "Yes" || married === "YES") {
+//             console.log("You are eligible to vote.");
+//             alert("You are eligible to vote.");
+//         } else {
+//             console.log("You are not eligible to vote.");
+//             alert("You are not eligible to vote.");
+//         }
+//     } else {
+//         console.log("You are not eligible to vote.");
+//         alert("You are not eligible to vote.");
+//     }
+// } else {
+//     console.log("You are not eligible to vote.");
+//     alert("You are not eligible to vote.");
+// }
+
+// Ask for user input
+// var nationality = prompt("What is your nationality? Enter 1 for Pakistani, 2 for Indian, or 3 for non-Pakistani/non-Indian.");
+// var gender = prompt("What is your gender? Enter 1 for male or 2 for female.");
+// var age = Number(prompt("What is your age?"));
+
+// // Check if the person is eligible to vote
+// if (nationality === "1" || nationality === "2") {
+//   if (gender === "1") {
+//     if (age >= 18) {
+//       alert("You are eligible to vote!");
+//     } else {
+//       alert("You are not eligible to vote.");
+//     }
+//   } else if (gender === "2") {
+//     var married = prompt("Are you married? Enter 1 for yes or 2 for no.");
+//     if (married === "1") {
+//       alert("You are eligible to vote!");
+//     } else if (married === "2") {
+//       alert("You are not eligible to vote.");
+//     } else {
+//       alert("Invalid input.");
+//     }
+//   } else {
+//     alert("Invalid input.");
+//   }
+// } else if (nationality === "3") {
+//   alert("You are not eligible to vote.");
+// } else {
+//   alert("Invalid input.");
+// }
+
+
+// agr code ni mila tw ye best ha
+
+// var nationality = prompt("Enter your nationality (1 for Pakistani, 2 for Indian):");
+// var gender = prompt("Enter your gender (1 for Male, 2 for Female):");
+// var age = Number(prompt("Enter your age:"));
+
+// if (nationality == 1 || nationality == 2) {
+//   if (gender == 1 && age >= 18) {
+//     console.log("You are eligible to vote!");
+//   } else if (gender == 2 && age >= 18) {
+//     var maritalStatus = prompt("Are you married? (1 for Yes, 2 for No):");
+//     if (maritalStatus == 1) {
+//       console.log("You are eligible to vote!");
+//     } else if (maritalStatus == 2) {
+//       console.log("You are not eligible to vote.");
+//     } else {
+//       console.log("Invalid input.");
+//     }
+//   } else {
+//     console.log("You are not eligible to vote.");
+//   }
+// } else {
+//   console.log("You are not eligible to vote.");
+// }
+
+
+// let nationality = prompt("What is your nationality? (Enter 1 for Pakistani or 2 for Indian)");
+// let gender = prompt("What is your gender? (Enter 1 for Male or 2 for Female)");
+// let age = Number(prompt("What is your age?"));
+
+// if (nationality === "1" || nationality === "2") {
+//   if (gender === "1") {
+//     if (age >= 18) {
+//       alert("You are eligible to vote.");
+//     } else {
+//       alert("You are not eligible to vote.");
+//     }
+//   } else if (gender === "2") {
+//     let maritalStatus = prompt("Are you married? (Enter 1 for Yes or 2 for No)");
+//     if (maritalStatus === "1") {
+//       alert("You are eligible to vote.");
+//     } else if (maritalStatus === "2") {
+//       alert("You are not eligible to vote.");
+//     } else {
+//       alert("Invalid input. Please try again.");
+//     }
+//   } else {
+//     alert("Invalid input. Please try again.");
+//   }
+// } else {
+//   alert("You are not eligible to vote.");
+// }
+
+
+// var nationality = prompt("What is your nationality? Enter 1 for Pakistani and 2 for Indian:");
+// var gender = prompt("What is your gender? Enter 1 for male and 2 for female:");
+// var age = Number(prompt("What is your age?"));
+
+// if (nationality == 1) {
+//   if (gender == 1 && age >= 18) {
+//     alert("You are eligible to vote!");
+//   } else if (gender == 2 && age >= 18) {
+//     var isMarried = prompt("Are you married? Enter 1 for yes and 2 for no:");
+//     if (isMarried == 1) {
+//       alert("You are eligible to vote!");
+//     } else {
+//       alert("You are not eligible to vote.");
+//     }
+//   } else {
+//     alert("You are not eligible to vote.");
+//   }
+// } else {
+//   alert("You are not eligible to vote.");
+// }
+
+// var WorldCupSquad = ["babar", "shaheen", "rizwan", "shadab", "ihsanullah", "naseem", "fakhar", "hasnain", "shahid afridi", "asif", "saim", "rauf", "haris", "malik", "ifti"];
+
+// var WorldCupSquadCopy = WorldCupSquad.slice();
+
+// var finalTeamPlayers = [];
+// for (var i = 0; i < 11; i++) {
+//   var player = WorldCupSquadCopy.pop();
+//   finalTeamPlayers.push(player);
+// }
+// console.log(WorldCupSquad);
+// alert(WorldCupSquad);
+// console.log(finalTeamPlayers);
+// alert(finalTeamPlayers)
+
+// let WorldCupSquad = ["Babar Azam", "Fakhar Zaman", "Imam-ul-Haq", "Shadab Khan", "Mohammad Hafeez", "Shoaib Malik", "Haris Sohail", "Sarfraz Ahmed", "Imad Wasim", "Hasan Ali", "Wahab Riaz", "Shaheen Afridi", "Mohammad Amir", "Asif Ali", "Junaid Khan"];
+
+// let finalTeam = WorldCupSquad.slice(0,11);
+// alert("Squad of Players: " + WorldCupSquad.join(", "));
+// alert("Final Team Players: " + finalTeam.join(", "));
+
+
+// // Define the array of players
+// const WorldCupSquad = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9", "Player 10", "Player 11", "Player 12", "Player 13", "Player 14", "Player 15"];
+
+// // Make a copy of the array to avoid modifying the original
+// const finalTeam = WorldCupSquad.slice(0, 11);
+
+// // Output the final team in an alert box
+// alert("The final team for tomorrow's match against India is:\n" + finalTeam.join("\n"));
+
+// // Pakistani World Cup Squad (15 players)
+// const WorldCupSquad = ['Babar Azam', 'Fakhar Zaman', 'Imam-ul-Haq', 'Asif Ali', 'Haris Sohail', 'Mohammad Hafeez', 'Shadab Khan', 'Imad Wasim', 'Sarfaraz Ahmed (c&wk)', 'Mohammad Amir', 'Hasan Ali', 'Shaheen Afridi', 'Wahab Riaz', 'Junaid Khan', 'Shoaib Malik'];
+
+// // Copy of Pakistani World Cup Squad for playing 11
+// const PakistanPlaying11 = WorldCupSquad.slice(0, 11);
+
+// // Alert for Pakistani World Cup Squad
+// alert('Pakistani World Cup Squad: ' + WorldCupSquad.join(', '));
+
+// // Alert for Pakistani Playing 11 against India
+// alert('Pakistani Playing 11 against India: ' + PakistanPlaying11.join(', '));
+
+// var num=+prompt("enter a number")
+// if(num%3===0 && num%5===0){
+//     console.log("both3&5");
+// }
+// else if(num%3===0){console.log("only 3");
+
+// }
+// else if(num%5===0){
+//     console.log("only 5");
+// }
+
+// console.log(null||false);
+
+// var num=+prompt("enter a number")
+// if (num%3===0 && num%5===0) {console.log("both 3&5")
+    
+// }
+// else if (num%3===0){
+//     console.log("3 only")
+// }
+// else if (num%5===0) {console.log("5only")
+    
+// }
+// var num=+prompt("Type your percentage");
+// var grade;
+
+// for loops_________________________________________________
+// var table = prompt("enter table")
+// for (var i = 1; i <=10; i++){
+//     console.log(table+ "x" + i+ "="+ i*table);
+// }
+
+// var table=prompt("Enter A Number")
+// for(i=1; i<=10; i++){
+//     console.log(table + "x"+i+"="+ i*table);
+// }
+
+
+// var cities = ["Karachi","Lahore","Peshawer","Islamabad",];
+// var city = prompt("Write City Name");
+
+// for (var i=0; i=cities.length;i++){
+//     if(city===cities[i]){
+//         console.log("found"+city);
+//     }
+// }
+
+// var cities = ["Karachi", "Lahore", "Peshawar", "Islamabad"];
+// var city = prompt("Write a city name");
+// var found = false;
+
+// for (var i = 0; i < cities.length; i++) {
+//     if (city.toLowerCase() === cities[i].toLowerCase()) {
+//         console.log("Found " + city);
+//         found = true;
+//         break;
+//     }
+// }
+
+// if (!found) {
+//     console.log("Not Found " + city);
+// }
+
+// var cities = ["Karachi", "Lahore", "Peshawar", "Islamabad"];
+// var city = prompt("Write a city name");
+
+// for (var i = 0; i < cities.length; i++) {
+//     if (city === cities[i]) {
+//         console.log("Found " + city);
+        
+        
+//     }
+// }
+
+// var cities = ["Karachi","Lahore","Peshawer","Islamabad",];
+// var city = prompt("Write City Name");
+// var foun=false
+
+// for (var i=0; i<cities.length;i++){
+//     if(city===cities[i]){
+//         console.log("found" +city);
+//         var foun=true
+//         break;
+//     }else {
+//         console.log("Not found " +city);
+//     }
+// }
+
+final revision
