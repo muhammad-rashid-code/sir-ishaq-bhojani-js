0ca4153f7214739f207d29670703bcd6a09e328f
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-04-29 08:39:28 +0500

    java scipt

--- revision2/app.js
+++ revision2/app.js
@@ -1,3 +1,39 @@
+// var num = 5;
+// alert(num--);
+// alert(num);
+
+// var num = 5;
+// ++num; // 6
+// var num2 = num--; // 6
+// alert(++num + num2); //12
+
+// var num = 5;
+// var text = "Hello";
+// var text2 = 5;
+// // 5+5+5
+// console.log( text2 + num + text);
+
+// var num = +prompt("What is your fav number?", 5);
+// console.log(num + 5);
+
+// COMPARISON OPERATORS
+
+// var num = 5;
+// var num2 = 10;
+
+// "5" == 5 // true;
+// "5" === 5 // false;
+ 
+// console.log(num !== num2);
+
+// var num = +prompt("What is your fav number?", 5);
+
+// if (num % 2 === 0) {
+//     console.log("This is an even number");
+// } else {
+//     console.log("This is an odd number");
+// }
+
 // chapter#6 Math expressions:
 // Unfamiliar operators
 
@@ -56,4 +92,92 @@
 // ++num;
 // var num2=num--;
 // alert(++num + num2)  must do a practice
+
+// Math expressions:
+// Eliminating ambiguity  13 :27
+
+// concatenation
+// var text="hello"
+// var text2="rashid"
+// alert(text+text2)
+
+// var text="hello";
+// var text2=5;
+// alert(text+text2);
+
+// var num=6;
+// var text="hello";
+// var text2=5;
+// alert(text+text2+num);
+
+// var num=6;
+// var text="hello";
+// var text2=5;
+// alert(text+(text2+num));
+
+// var num=6;
+// var text="hello";
+// var text2=5;
+// alert(text2+num+text);
+
+// chapter#8Prompts_______________________
+
+// var username=prompt("What Is Your Name")
+// console.log("Hello "+username)
+
+// var username=prompt("What Is Your Name")
+// alert("Hello "+username)
+
+// console.log("Hello "+username)
+
+// var username=+prompt("What Is Your FAVRITE number",6);
+// console.log(username+5);
+
+// var username=+prompt("What Is Your FAVRITE number",6);
+// num = parseInt(username);
+// console.log(username+5);
+
+// var username=+prompt("What Is Your FAVRITE number",6);
+// num=Number(username)
+// console.log(username+5); 
+// 3 ways to make prompt a number
+
+// comparision oprators
+// == no firqa wariyt 
+// === firqa wariyt type checkin
+
+// "5"==5 true string==Number
+// "5"===5 false 
+
+// var num=5
+// var num2="5"
+// console.log(num==num2) true
+
+// var num=5
+// var num2="5"
+// console.log(num===num2) false
+
+// var num=5;
+// var num2=6;
+// console.log(num>num2);
+
+// var num=5;
+// var num2=5;
+// console.log(num>=num2);
+
+// var num=5;
+// var num2=6;
+// console.log(num!==num2);
+
+// var num=5;
+// var num2=5;
+// console.log(num!==num2);
+
+// var num=+prompt("What Is Your Favrite number",5)
+// if(num%2===0){
+//     console.log("this is an even number");
+// }else{
+//     console.log  ("this is an odd number")
+// }
+
 
