158356366ae7337a3916fc9a5ff317fa1e569fed
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-04-29 05:37:39 +0500

    java scipt 2

--- revision2/app.js
+++ revision2/app.js
@@ -10,3 +10,50 @@
 // num=num+6
 // alert(num)
 
+// begins 
+// var num=5
+// num=num+5
+// alert(num)
+
+// or 
+
+// var num=5
+// num+=5
+// alert(num)         14to16 & 20to22 both are same 
+
+// increament(pre-increament"++5"(post-increament"5++"")) & decrement (++) bachpan me calculator(8*1=8|+8) me use kia tha 
+// var num=5
+// num++=5post-increament
+// ++num=pre-increament
+// var num=5;
+// alert(num--);
+// alert(num);  post increment(samajne k lye postmotom)
+
+// var num=5
+// alert(--num) pre increment
+
+// var num=5
+// alert(++num) pre increment
+
+// var num=5;
+
+// // ++num;
+// alert(num++);
+// // alert(num--)
+// // alert(++num)
+
+// var num=5;
+// ++num;
+// alert(num--);
+// alert(++num);
+
+// sir 
+// var num=5;
+// ++num;
+// alert(num--);
+
+// var num=5
+// ++num;
+// var num2=num--;
+// alert(++num + num2)  must do a practice
+
